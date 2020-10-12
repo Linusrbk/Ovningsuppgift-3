93e7761e4f3049cf1d3133ed053986ab663ff0cc
Linusrbk <linus.astrom@elev.ga.ntig.se>
2020-10-12 15:00:57 +0200

    ja

--- 3,1.py
+++ 3,1.py
@@ -1,6 +1,6 @@
 a = int(input('hur mycket ringer du per månad i minuter:'))#beräknar vilket abonnemang du borde använda beroende på hur mycket du ringer 
 if a> 0 and a < 34:#printar ifall det är högre än 0 minuter och mindre än 34 minuter med en and som änvänds så att både den ska vara störe enen sak och mindre en annnan
-    print('kör på abonnemanget Konstant')
+    print('kör på abonnemanget Konstant')#en print vad mer vill du veta?
 elif a> 33 and a < 67:#printar ifall det är högre än 33 minuter och mindre än 67 minuter med en and som änvänds så att både den ska vara störe enen sak och mindre en annnan
     print('kör på abonnemanget Normal')
 elif a> 66 and a <43830:#printar ifall det är högre än 66 minuter och mindre än 43830 minuter med en and som änvänds så att både den ska vara störe enen sak och mindre en annnan
---
+++ 3,3.py
@@ -0,0 +1,4 @@
+import.math
+a = float(input('första sidan:'))#för att få vad dom olika sidorna är 
+b = float(input('andra sidan:'))
+c = math.sqrt(a**2 + b**2 - 2ab * cos (a) ) # math.sqrt är square root och sen är det av denna ekvation 
