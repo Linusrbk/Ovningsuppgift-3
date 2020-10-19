78afede4476dd1b50b5c023f72f5f6f1ab69d508
Linusrbk <linus.astrom@elev.ga.ntig.se>
2020-10-19 12:32:19 +0200

    Update 3,3.py

--- 3,3.py
+++ 3,3.py
@@ -1,4 +1,4 @@
 import.math
 a = float(input('första sidan:'))#för att få vad dom olika sidorna är 
 b = float(input('andra sidan:'))
-c = math.sqrt(a**2 + b**2 - 2ab * cos (a) ) # math.sqrt är square root och sen är det av denna ekvation 
+c = math.sqrt(a**2 + b**2 - 2* a * b * math.cos (alfa) ) # math.sqrt är square root och sen är det av denna ekvation 
